860f365e165cbe776cd90b2af5bcb1812278e794
Andrew <theandyw19@gmail.com>
2021-07-23 16:48:32 +0100

    1.5 Loot options

--- src/main/java/me/andyw19/damagedrops/DamageListener.java
+++ src/main/java/me/andyw19/damagedrops/DamageListener.java
@@ -10,6 +10,9 @@ import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.List;
+import java.util.Random;
+
 public class DamageListener implements Listener {
 
     private DamageDrops damageDrops;
@@ -29,14 +32,30 @@ public class DamageListener implements Listener {
     public final Material [] ironToNetheriteAxeSword = {Material.IRON_AXE, Material.IRON_SWORD, Material.GOLDEN_AXE, Material.GOLDEN_SWORD, Material.DIAMOND_AXE, Material.DIAMOND_SWORD,
     Material.NETHERITE_AXE, Material.NETHERITE_SWORD};
 
-    private final Material [] zombieLoot = {Material.ROTTEN_FLESH};
-    private final Material [] skeletonLoot = {Material.BONE, Material.BONE_MEAL};
-    private final Material [] creeperLoot = {Material.GUNPOWDER, Material.FIRE_CHARGE, Material.TNT};
-    private final Material [] golemLoot = {Material.RAW_IRON, Material.POPPY};
-    private final Material [] beeLoot = {Material.HONEYCOMB};
-    private final Material [] blazeLoot = {Material.BLAZE_ROD, Material.BLAZE_ROD, Material.BLAZE_ROD};
+    private final List<Material> creeperCommonLoot = DamageDrops.getSettings().creeperCommonMatList;
+    private final List<Material> creeperMediumLoot = DamageDrops.getSettings().creeperMediumMatList;
+    private final List<Material> creeperRareLoot = DamageDrops.getSettings().creeperRareMatList;
 
-    private double [] lootLevelArray = {0.25, 0.1, 0.03};
+    private final List<Material> zombieCommonLoot = DamageDrops.getSettings().zombieCommonMatList;
+    private final List<Material> zombieMediumLoot = DamageDrops.getSettings().zombieMediumMatList;
+    private final List<Material> zombieRareLoot = DamageDrops.getSettings().zombieRareMatList;
+
+    private final List<Material> skeletonCommonLoot = DamageDrops.getSettings().skeletonCommonMatList;
+    private final List<Material> skeletonMediumLoot = DamageDrops.getSettings().skeletonMediumMatList;
+    private final List<Material> skeletonRareLoot = DamageDrops.getSettings().skeletonRareMatList;
+
+    private final List<Material> golemCommonLoot = DamageDrops.getSettings().golemCommonMatList;
+    private final List<Material> golemMediumLoot = DamageDrops.getSettings().golemMediumMatList;
+    private final List<Material> golemRareLoot = DamageDrops.getSettings().golemRareMatList;
+
+    private final List<Material> blazeCommonLoot = DamageDrops.getSettings().blazeCommonMatList;
+    private final List<Material> blazeMediumLoot = DamageDrops.getSettings().blazeMediumMatList;
+    private final List<Material> blazeRareLoot = DamageDrops.getSettings().blazeRareMatList;
+
+    private final List<Material> beeCommonLoot = DamageDrops.getSettings().beeCommonMatList;
+    private final List<Material> beeMediumLoot = DamageDrops.getSettings().beeMediumMatList;
+    private final List<Material> beeRareLoot = DamageDrops.getSettings().beeRareMatList;
+
     private final double [] creeperLootChance = DamageDrops.getSettings().getCreeperDropChance();
     private final double [] zombieLootChance = DamageDrops.getSettings().getZombieDropChance();
     private final double [] skeletonLootChance = DamageDrops.getSettings().getSkeletonDropChance();
@@ -44,34 +63,46 @@ public class DamageListener implements Listener {
     private final double [] blazeLootChance = DamageDrops.getSettings().getBlazeDropChance();
     private final double [] beeLootChance = DamageDrops.getSettings().getBeeDropChance();
 
+    private final int creeperLootLevel = DamageDrops.getSettings().getCreeperLootLevel();
+    private final int zombieLootLevel = DamageDrops.getSettings().getZombieLootLevel();
+    private final int skeletonLootLevel = DamageDrops.getSettings().getSkeletonLootLevel();
+    private final int golemLootLevel = DamageDrops.getSettings().getGolemLootLevel();
+    private final int blazeLootLevel = DamageDrops.getSettings().getBlazeLootLevel();
+    private final int beeLootLevel = DamageDrops.getSettings().getBeeLootLevel();
 
     @EventHandler
     public void onEntityDamage(EntityDamageEvent event) {
 
         switch (event.getEntity().getType()) {
             case CREEPER:
-                creeperCanPass = mobCheck(event, creeperCanPass, ironToNetheriteAxeSword, DamageDrops.getSettings().isCreeperDrops(), 1, 2, 3, creeperLoot, true, creeperLootChance);
+                creeperCanPass = mobCheck(event, creeperCanPass, ironToNetheriteAxeSword, DamageDrops.getSettings().isCreeperDrops(),DamageDrops.getSettings().getCreeperHighLoot(), creeperLootLevel,
+                        creeperCommonLoot, creeperMediumLoot, creeperRareLoot, true, creeperLootChance);
                 break;
             case ZOMBIE: case ZOMBIE_VILLAGER: case ZOMBIE_HORSE: case DROWNED:
-                zombieCanPass = mobCheck(event, zombieCanPass, ironToNetheriteAxeSword, DamageDrops.getSettings().isZombieDrops(), 1, 2, 1, zombieLoot, true, zombieLootChance);
+                zombieCanPass = mobCheck(event, zombieCanPass, ironToNetheriteAxeSword, DamageDrops.getSettings().isZombieDrops(), DamageDrops.getSettings().getZombieHighLoot(), zombieLootLevel,
+                        zombieCommonLoot, zombieMediumLoot, zombieRareLoot, true, zombieLootChance);
                 break;
             case SKELETON: case SKELETON_HORSE: case WITHER_SKELETON:
-                skeletonCanPass = mobCheck(event, skeletonCanPass, ironToNetheriteAxeSword, DamageDrops.getSettings().isSkeletonDrops(), 1, 2, 2, skeletonLoot, true, skeletonLootChance);
+                skeletonCanPass = mobCheck(event, skeletonCanPass, ironToNetheriteAxeSword, DamageDrops.getSettings().isSkeletonDrops(), DamageDrops.getSettings().getSkeletonHighLoot(), skeletonLootLevel,
+                        skeletonCommonLoot, skeletonMediumLoot, skeletonRareLoot, true, skeletonLootChance);
                 break;
             case IRON_GOLEM:
-                golemCanPass = mobCheck(event, golemCanPass, ironToNetheritePickaxe, DamageDrops.getSettings().isGolemDrops(), 1, 2, 2, golemLoot, true, golemLootChance);
+                golemCanPass = mobCheck(event, golemCanPass, ironToNetheritePickaxe, DamageDrops.getSettings().isGolemDrops(), DamageDrops.getSettings().getGolemHighLoot(), golemLootLevel,
+                        golemCommonLoot, golemMediumLoot, golemRareLoot, true, golemLootChance);
                 break;
             case BEE:
-                beeCanPass = mobCheck(event, beeCanPass, ironToNetheritePickaxe, DamageDrops.getSettings().isBeeDrops(), 1, 1, 1, beeLoot, false, beeLootChance);
+                beeCanPass = mobCheck(event, beeCanPass, ironToNetheritePickaxe, DamageDrops.getSettings().isBeeDrops(), DamageDrops.getSettings().getBeeHighLoot(), beeLootLevel,
+                        beeCommonLoot, beeMediumLoot, beeRareLoot, false, beeLootChance);
                 break;
             case BLAZE:
-                blazeCanPass = mobCheck(event, blazeCanPass, ironToNetheriteAxeSword, DamageDrops.getSettings().isBlazeDrops(), 1, 1, 1, blazeLoot, true, blazeLootChance);
+                blazeCanPass = mobCheck(event, blazeCanPass, ironToNetheriteAxeSword, DamageDrops.getSettings().isBlazeDrops(), DamageDrops.getSettings().getBlazeHighLoot(), blazeLootLevel,
+                        blazeCommonLoot, blazeMediumLoot, blazeRareLoot, true, blazeLootChance);
                 break;
         }
     }
 
-    private boolean mobCheck(EntityDamageEvent event, boolean canPass, Material[] matArray, boolean isEnabled, int lootNum, int highestLootNum, int lootLevel,
-                             Material [] lootList, boolean checkTool, double [] lootLevelArray) {
+    private boolean mobCheck(EntityDamageEvent event, boolean canPass, Material[] matArray, boolean isEnabled, int highestLootNum, int lootLevel,
+                             List<Material> commonLoot, List<Material> mediumLoot, List<Material> rareLoot, boolean checkTool, double [] lootLevelArray) {
         if (event.getCause() == EntityDamageEvent.DamageCause.ENTITY_ATTACK) {
             if (isEnabled) {
                 if (!(event.getEntity() instanceof CreatureSpawner) || DamageDrops.getSettings().isSpawnerDrops()) {
@@ -85,7 +116,7 @@ public class DamageListener implements Listener {
                             if (p.getEquipment().getItemInMainHand().getType().equals(mat) || !(checkTool)) {
                                 Location loc = event.getEntity().getLocation();
 
-                                loot(event, loc, lootNum, highestLootNum, lootLevel, lootList, lootLevelArray);
+                                loot(event, loc, highestLootNum, lootLevel, lootLevelArray, commonLoot, mediumLoot, rareLoot);
                                 break;
                             }
                         }
@@ -101,36 +132,28 @@ public class DamageListener implements Listener {
         return canPass;
     }
 
-    private void loot(EntityDamageEvent event, Location loc, int lootNum, int highestLootNum, int lootLevel, Material [] lootList, double [] lootLevelArray) {
+    private void loot(EntityDamageEvent event, Location loc, int highestLootNum, int lootLevel, double [] lootLevelArray, List<Material> commonLoot, List<Material> mediumLoot, List<Material> rareLoot) {
         double lootChance = Math.random();
-        double lootNumChance = Math.random();
-        int finalLootNum = lootNum;
+        int finalLootNum;
+
         switch (lootLevel) {
-            case 1:
-                if (lootChance < lootLevelArray[0]) {
-                    if (lootNumChance < 0.2) {finalLootNum = highestLootNum;}
-                    event.getEntity().getWorld().dropItemNaturally(loc, new ItemStack(lootList[0], finalLootNum));
-                }
-                break;
-            case 2:
-                if (lootChance < lootLevelArray[1]) {
-                    if (lootNumChance < 0.2) {finalLootNum = highestLootNum;}
-                    event.getEntity().getWorld().dropItemNaturally(loc, new ItemStack(lootList[1], finalLootNum));
-                } else if (lootChance < lootLevelArray[0]) {
-                    if (lootNumChance < 0.2) {finalLootNum = highestLootNum;}
-                    event.getEntity().getWorld().dropItemNaturally(loc, new ItemStack(lootList[0], finalLootNum));
-                }
-                break;
             case 3:
                 if (lootChance < lootLevelArray[2]) {
-                    if (lootNumChance < 0.2) {finalLootNum = highestLootNum;}
-                    event.getEntity().getWorld().dropItemNaturally(loc, new ItemStack(lootList[2], finalLootNum));
-                } else if (lootChance < lootLevelArray[1]) {
-                    if (lootNumChance < 0.2) {finalLootNum = highestLootNum;}
-                    event.getEntity().getWorld().dropItemNaturally(loc, new ItemStack(lootList[1], finalLootNum));
-                } else if (lootChance < lootLevelArray[0]) {
-                    if (lootNumChance < 0.2) {finalLootNum = highestLootNum;}
-                    event.getEntity().getWorld().dropItemNaturally(loc, new ItemStack(lootList[0], finalLootNum));
+                    finalLootNum = getRandomNumber(1, highestLootNum);
+                    event.getEntity().getWorld().dropItemNaturally(loc, new ItemStack(getRandomElement(rareLoot), finalLootNum));
+                    break;
+                }
+            case 2:
+                if (lootChance < lootLevelArray[1]) {
+                    finalLootNum = getRandomNumber(1, highestLootNum);
+                    event.getEntity().getWorld().dropItemNaturally(loc, new ItemStack(getRandomElement(mediumLoot), finalLootNum));
+                    break;
+                }
+            case 1:
+                if (lootChance < lootLevelArray[0]) {
+                    finalLootNum = getRandomNumber(1, highestLootNum);
+                    event.getEntity().getWorld().dropItemNaturally(loc, new ItemStack(getRandomElement(commonLoot), finalLootNum));
+                    break;
                 }
                 break;
         }
@@ -149,6 +172,15 @@ public class DamageListener implements Listener {
             return true;
         }
         return canPass;
+    }
+
+    private int getRandomNumber(int min, int max) {
+        return (int) ((Math.random() * (max - min)) + min);
+    }
+
+    private Material getRandomElement(List<Material> list) {
+        Random rand = new Random();
+        return list.get(rand.nextInt(list.size()));
     }
 
 }
--- src/main/java/me/andyw19/damagedrops/config/Settings.java
+++ src/main/java/me/andyw19/damagedrops/config/Settings.java
@@ -1,8 +1,12 @@
 package me.andyw19.damagedrops.config;
 
 import me.andyw19.damagedrops.DamageDrops;
+import org.bukkit.Material;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Settings {
 
@@ -17,6 +21,44 @@ public class Settings {
     private boolean creeperDrops;
     private boolean beeDrops;
     private boolean blazeDrops;
+
+    private int creeperHighLoot;
+    private int zombieHighLoot;
+    private int skeletonHighLoot;
+    private int golemHighLoot;
+    private int beeHighLoot;
+    private int blazeHighLoot;
+
+    private int creeperLootLevel;
+    private int zombieLootLevel;
+    private int skeletonLootLevel;
+    private int golemLootLevel;
+    private int blazeLootLevel;
+    private int beeLootLevel;
+
+    public final List<Material> creeperCommonMatList = new ArrayList<>();
+    public final List<Material> creeperMediumMatList = new ArrayList<>();
+    public final List<Material> creeperRareMatList = new ArrayList<>();
+
+    public final List<Material> zombieCommonMatList = new ArrayList<>();
+    public final List<Material> zombieMediumMatList = new ArrayList<>();
+    public final List<Material> zombieRareMatList = new ArrayList<>();
+
+    public final List<Material> skeletonCommonMatList = new ArrayList<>();
+    public final List<Material> skeletonMediumMatList = new ArrayList<>();
+    public final List<Material> skeletonRareMatList = new ArrayList<>();
+
+    public final List<Material> golemCommonMatList = new ArrayList<>();
+    public final List<Material> golemMediumMatList = new ArrayList<>();
+    public final List<Material> golemRareMatList = new ArrayList<>();
+
+    public final List<Material> blazeCommonMatList = new ArrayList<>();
+    public final List<Material> blazeMediumMatList = new ArrayList<>();
+    public final List<Material> blazeRareMatList = new ArrayList<>();
+
+    public final List<Material> beeCommonMatList = new ArrayList<>();
+    public final List<Material> beeMediumMatList = new ArrayList<>();
+    public final List<Material> beeRareMatList = new ArrayList<>();
 
     private final double [] creeperDropChance = new double[3];
     private final double [] zombieDropChance = new double[3];
@@ -36,47 +78,105 @@ public class Settings {
 
     public void reloadFromFile(FileConfiguration config) {
         try {
+            config.options().header("#This is a test#\n#Line 2 Test#");
             this.dropChanceInterval = config.getLong("drop_chance_interval", 1000);
             this.spawnerDrops = config.getBoolean("spawnerdrops", false);
 
             ConfigurationSection creeper = config.getConfigurationSection("mobs.creeper");
             this.creeperDrops = creeper.getBoolean("creeperdrops", true);
+            this.creeperHighLoot = creeper.getInt("highestloot", 2);
             this.creeperDropChance[0] = creeper.getDouble("commonlootchance", 0.35);
             this.creeperDropChance[1] = creeper.getDouble("mediumlootchance", 0.15);
             this.creeperDropChance[2] = creeper.getDouble("rarelootchance", 0.02);
+            this.creeperLootLevel = creeper.getInt("loot.lootlevel", 3);
+            List<String> creeperCommonList = config.getStringList("mobs.creeper.loot.commonloot");
+            List<String> creeperMediumList = config.getStringList("mobs.creeper.loot.mediumloot");
+            List<String> creeperRareList = config.getStringList("mobs.creeper.loot.rareloot");
+            checkMat(creeperCommonList, creeperCommonMatList);
+            checkMat(creeperMediumList, creeperMediumMatList);
+            checkMat(creeperRareList, creeperRareMatList);
+
 
             ConfigurationSection zombie = config.getConfigurationSection("mobs.zombie");
             this.zombieDrops = zombie.getBoolean("zombiedrops", true);
+            this.zombieHighLoot = zombie.getInt("highestloot", 2);
             this.zombieDropChance[0] = zombie.getDouble("commonlootchance", 0.35);
             this.zombieDropChance[1] = zombie.getDouble("mediumlootchance", 0.15);
             this.zombieDropChance[2] = zombie.getDouble("rarelootchance", 0.02);
+            this.zombieLootLevel = zombie.getInt("loot.lootlevel", 3);
+            List<String> zombieCommonList = config.getStringList("mobs.zombie.loot.commonloot");
+            List<String> zombieMediumList = config.getStringList("mobs.zombie.loot.mediumloot");
+            List<String> zombieRareList = config.getStringList("mobs.zombie.loot.rareloot");
+            checkMat(zombieCommonList, zombieCommonMatList);
+            checkMat(zombieMediumList, zombieMediumMatList);
+            checkMat(zombieRareList, zombieRareMatList);
 
             ConfigurationSection skeleton = config.getConfigurationSection("mobs.skeleton");
             this.skeletonDrops = skeleton.getBoolean("skeletondrops", true);
+            this.skeletonHighLoot = skeleton.getInt("highestloot", 2);
             this.skeletonDropChance[0] = skeleton.getDouble("commonlootchance", 0.35);
             this.skeletonDropChance[1] = skeleton.getDouble("mediumlootchance", 0.15);
             this.skeletonDropChance[2] = skeleton.getDouble("rarelootchance", 0.02);
+            this.skeletonLootLevel = skeleton.getInt("loot.lootlevel", 3);
+            List<String> skeletonCommonList = config.getStringList("mobs.skeleton.loot.commonloot");
+            List<String> skeletonMediumList = config.getStringList("mobs.skeleton.loot.mediumloot");
+            List<String> skeletonRareList = config.getStringList("mobs.skeleton.loot.rareloot");
+            checkMat(skeletonCommonList, skeletonCommonMatList);
+            checkMat(skeletonMediumList, skeletonMediumMatList);
+            checkMat(skeletonRareList, skeletonRareMatList);
 
             ConfigurationSection irongolem = config.getConfigurationSection("mobs.irongolem");
             this.golemDrops = irongolem.getBoolean("irongolemdrops", true);
+            this.golemHighLoot = irongolem.getInt("highestloot", 2);
             this.golemDropChance[0] = irongolem.getDouble("commonlootchance", 0.35);
             this.golemDropChance[1] = irongolem.getDouble("mediumlootchance", 0.15);
             this.golemDropChance[2] = irongolem.getDouble("rarelootchance", 0.02);
+            this.golemLootLevel = irongolem.getInt("loot.lootlevel", 3);
+            List<String> golemCommonList = config.getStringList("mobs.golem.loot.commonloot");
+            List<String> golemMediumList = config.getStringList("mobs.golem.loot.mediumloot");
+            List<String> golemRareList = config.getStringList("mobs.golem.loot.rareloot");
+            checkMat(golemCommonList, golemCommonMatList);
+            checkMat(golemMediumList, golemMediumMatList);
+            checkMat(golemRareList, golemRareMatList);
 
             ConfigurationSection blaze = config.getConfigurationSection("mobs.blaze");
             this.blazeDrops = blaze.getBoolean("blazedrops", true);
+            this.blazeHighLoot = blaze.getInt("highestloot", 2);
             this.blazeDropChance[0] = blaze.getDouble("commonlootchance", 0.35);
             this.blazeDropChance[1] = blaze.getDouble("mediumlootchance", 0.15);
             this.blazeDropChance[2] = blaze.getDouble("rarelootchance", 0.02);
+            this.blazeLootLevel = blaze.getInt("loot.lootlevel", 3);
+            List<String> blazeCommonList = config.getStringList("mobs.blaze.loot.commonloot");
+            List<String> blazeMediumList = config.getStringList("mobs.blaze.loot.mediumloot");
+            List<String> blazeRareList = config.getStringList("mobs.blaze.loot.rareloot");
+            checkMat(blazeCommonList, blazeCommonMatList);
+            checkMat(blazeMediumList, blazeMediumMatList);
+            checkMat(blazeRareList, blazeRareMatList);
 
             ConfigurationSection bee = config.getConfigurationSection("mobs.bee");
             this.beeDrops = bee.getBoolean("beedrops", true);
+            this.beeHighLoot = bee.getInt("highestloot", 1);
             this.beeDropChance[0] = bee.getDouble("commonlootchance", 0.35);
             this.beeDropChance[1] = bee.getDouble("mediumlootchance", 0.15);
             this.beeDropChance[2] = bee.getDouble("rarelootchance", 0.02);
+            this.beeLootLevel = bee.getInt("loot.lootlevel", 3);
+            List<String> beeCommonList = config.getStringList("mobs.bee.loot.commonloot");
+            List<String> beeMediumList = config.getStringList("mobs.bee.loot.mediumloot");
+            List<String> beeRareList = config.getStringList("mobs.bee.loot.rareloot");
+            checkMat(beeCommonList, beeCommonMatList);
+            checkMat(beeMediumList, beeMediumMatList);
+            checkMat(beeRareList, beeRareMatList);
 
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    private void checkMat(List<String> stringList, List<Material> matList) {
+        for(String material : stringList) {
+            Material mat = Material.getMaterial(material);
+            if (material == null) continue;
+            matList.add(mat);
         }
     }
 
@@ -115,4 +215,51 @@ public class Settings {
     public double [] getGolemDropChance(){ return golemDropChance; }
     public double [] getBlazeDropChance(){ return blazeDropChance; }
     public double [] getBeeDropChance(){ return beeDropChance; }
+
+    public int getCreeperHighLoot() {
+        return creeperHighLoot;
+    }
+    public int getZombieHighLoot() {
+        return zombieHighLoot;
+    }
+
+    public int getSkeletonHighLoot() {
+        return skeletonHighLoot;
+    }
+
+    public int getGolemHighLoot() {
+        return golemHighLoot;
+    }
+
+    public int getBeeHighLoot() {
+        return beeHighLoot;
+    }
+
+    public int getBlazeHighLoot() {
+        return blazeHighLoot;
+    }
+
+    public int getCreeperLootLevel() {
+        return creeperLootLevel;
+    }
+
+    public int getZombieLootLevel() {
+        return zombieLootLevel;
+    }
+
+    public int getSkeletonLootLevel() {
+        return skeletonLootLevel;
+    }
+
+    public int getGolemLootLevel() {
+        return golemLootLevel;
+    }
+
+    public int getBlazeLootLevel() {
+        return blazeLootLevel;
+    }
+
+    public int getBeeLootLevel() {
+        return beeLootLevel;
+    }
 }
